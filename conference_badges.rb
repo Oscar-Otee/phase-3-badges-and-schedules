b21ec50b5afc58cfc4d946756ed4eeb1da117571
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-08-13 20:15:23 +0300

    Assignment completed successfully

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(array)
+    array.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(array)
+    array.map do |name| 
+        puts "Hello, my name is #{name}."
+    end
+
+    array.map.with_index do |name, index|
+        puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+   
+end
